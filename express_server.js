0959bb42ff9b96ad884da1e7542c679b8c5a7da6
dylanhamada <dylan.hamada@gmail.com>
2023-10-31 00:19:38 -0400

    Implement GET route to /urls/new and POST route to /urls
    - Write generateRandomString to create new short URL

--- express_server.js
+++ express_server.js
@@ -3,11 +3,21 @@ const app = express();
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+const generateRandomString = () => {
+  let randStr = "";
+  const alphaChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
+  for (let i = 0; i < 6; i++) {
+    randStr += alphaChars[Math.floor(Math.random() * alphaChars.length)];
+  }
+  return randStr;
+}
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -26,9 +36,18 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
